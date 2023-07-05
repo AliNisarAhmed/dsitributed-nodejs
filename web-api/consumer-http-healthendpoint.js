69e85d9059cbf14dbabecadf27184e1a5184d390
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2023-07-04 21:34:29 -0600

    chap3 Scaling: node cluster module vs HAProxy

---
+++ web-api/consumer-http-healthendpoint.js
@@ -0,0 +1,28 @@
+import Fastify from 'fastify';
+import fetch from 'node-fetch';
+
+const server = Fastify({
+  logger: true,
+});
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.target || "localhost:4000";
+
+server.get("/", async () => {
+  const req = await fetch(`http://${TARGET}/recipes/42`);
+  const producer_data = await req.json();
+
+  return {
+    consumer_pid: process.pid,
+    producer_data,
+  };
+});
+
+server.get('/health', async () => {
+  console.log('health check');
+  return 'OK';
+})
+
+server.listen({ port: PORT, host: HOST }, () => {
+  console.log(`Consumer running at http://${HOST}:${PORT}/`);
+});
